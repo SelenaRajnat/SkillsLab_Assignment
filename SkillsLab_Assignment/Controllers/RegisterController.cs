78a536a07550aefad62867ecfb1bfd205c7d7daf
Selena <Selena.Rajnat@ceridian.com>
2023-12-18 13:48:39 +0400

    Code Review

--- SkillsLab_Assignment/Controllers/RegisterController.cs
+++ SkillsLab_Assignment/Controllers/RegisterController.cs
@@ -23,22 +23,6 @@ namespace SkillsLab_Assignment.Controllers
         {
             return View();
         }
-        //public ActionResult Verify(Employee employee)
-        //{
-        //    List<ValidationResult> validationResults = _register.RegisterResults(employee);
-
-        //    if (validationResults.Count == 0)
-        //    {
-        //        return View("Success");
-        //    }
-        //    else
-        //    {
-        //        validationResults.Add(new ValidationResult("Unable to register user!"));
-        //    }
-
-        //    return View("Error", validationResults);
-
-        //}
         public ActionResult Verify(Employee employee)
         {
             if(_register.RegisterResults(employee))
@@ -46,78 +30,5 @@ namespace SkillsLab_Assignment.Controllers
            
             return RedirectToAction("Index", "Register");
         }
-
-
-        //// GET: Register/Details/5
-        //public ActionResult Details(int id)
-        //{
-        //    return View();
-        //}
-
-        //// GET: Register/Create
-        //public ActionResult Success()
-        //{
-        //    return View();
-        //}
-
-        //// POST: Register/Create
-        //[HttpPost]
-        //public ActionResult Create(FormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add insert logic here
-
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //// GET: Register/Edit/5
-        //public ActionResult Edit(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: Register/Edit/5
-        //[HttpPost]
-        //public ActionResult Edit(int id, FormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add update logic here
-
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //// GET: Register/Delete/5
-        //public ActionResult Delete(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: Register/Delete/5
-        //[HttpPost]
-        //public ActionResult Delete(int id, FormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add delete logic here
-
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
     }
 }
